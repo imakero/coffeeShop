8841a70c753773fab69ed6022d9917bdb0286f24
imakero <45946863+imakero@users.noreply.github.com>
2021-09-27 15:45:53 +0200

    fix: clean up code

--- index.js
+++ index.js
@@ -13,9 +13,9 @@ const coffeeForm = document.getElementById('coffee-form')
 coffeeForm.addEventListener('submit', onSubmit)
 
 coffees.forEach((coffee, index) => {
-  const coffeeOption = document.createElement('option')
-  coffeeOption.innerText = `${coffee.name} - ${coffee.price} kr`
-  coffeeOption.value = index
+  const coffeeOption = document.createElement('option') //<option></option>
+  coffeeOption.innerText = `${coffee.name} - ${coffee.price} kr` //<option>Bryggkaffe - 20 kr</option>
+  coffeeOption.value = index //<option value="0">Bryggkaffe - 20 kr</option>
   select.appendChild(coffeeOption)
 })
 
@@ -31,39 +31,37 @@ class Customer {
       quantity: quantity,
     }
 
+    //console.log(JSON.stringify(this.transactions, null, 2))
     this.transactions.push(transaction)
+    //console.log(JSON.stringify(this.transactions, null, 2))
     this.displayTransaction(transaction)
-  }
-
-  transactionTotalPrice(transaction) {
-    return transaction.quantity * transaction.price
   }
 
   displayTransaction(transaction) {
     const transactionElement = document.createElement('p')
     const { quantity, name, price } = transaction
 
-    transactionElement.innerText = `Du köpte ${quantity} st ${name} för ${price} kr styck. Summa: ${this.transactionTotalPrice(
-      transaction
-    )} kr`
+    transactionElement.innerText = `Du köpte ${quantity} st ${name} för ${price} kr styck. Summa: ${
+      quantity * price
+    } kr`
 
     transactionsContainer.appendChild(transactionElement)
   }
 
-  calculateTotalSpent(){
+  calculateTotalSpent() {
     let sum = 0
     this.transactions.forEach((transaction) => {
-      const transactionTotal = this.transactionTotalPrice(transaction)
+      const transactionTotal = transaction.quantity * transaction.price
       sum += transactionTotal
-    })  
+    })
     return sum
   }
 
-  displayTotalSpent(){
+  displayTotalSpent() {
     totalSpent.innerText = `Du har handlat för ${this.calculateTotalSpent()} kr`
   }
 
-  calculateTotalCups(){
+  calculateTotalCups() {
     let totalCups = 0
     this.transactions.forEach((transaction) => {
       totalCups += transaction.quantity
@@ -71,13 +69,9 @@ class Customer {
     return totalCups
   }
 
-  displayMemberStatus(){
-    memberStatus.innerText = `Medlemskapsstatus: ${this.calculateTotalCups()}`
-  }
-
-  displayMemberStatusName(){
+  displayMemberStatus() {
     const totalCups = this.calculateTotalCups()
-    if (totalCups < 10){
+    if (totalCups < 10) {
       memberStatus.innerText = `Medlemskapsstatus: Brons`
     } else if (totalCups < 30) {
       memberStatus.innerText = `Medlemskapsstatus: Silver`
@@ -87,24 +81,20 @@ class Customer {
   }
 }
 
-
-
 function onSubmit(event) {
   event.preventDefault()
 
   const coffeeTypeSelect = document.getElementById('coffee-select')
-  const coffeeId = parseInt(coffeeTypeSelect.value)
+  const coffeeId = parseInt(coffeeTypeSelect.value) //Because value is of type string
 
   const quantityInput = document.getElementById('coffee-input')
   const quantity = parseInt(quantityInput.value)
+
   if (quantity > 0) {
     customer.addTransaction(coffeeId, quantity)
-
     customer.displayTotalSpent()
-  
-    customer.displayMemberStatusName()
+    customer.displayMemberStatus()
   } else {
-
   }
 }
 
